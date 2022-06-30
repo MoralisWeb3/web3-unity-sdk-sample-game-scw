b303f2deb76424e5ad2ec886a158d2b5c81fcbe2
Samuel Asher Rivello <74617375+srivelloBeamable@users.noreply.github.com>
2022-06-30 10:07:02 +0100

    Update ReferenceByGuidAttribute.cs

--- Unity/Assets/Moralis Web3 Unity SDK Samples/Shared/Scripts/Runtime/Attributes/ReferenceByGuidAttribute.cs
+++ Unity/Assets/Moralis Web3 Unity SDK Samples/Shared/Scripts/Runtime/Attributes/ReferenceByGuidAttribute.cs
@@ -2,8 +2,8 @@ using System;
 
 namespace MoralisUnity.Samples.Shared.Attributes
 {
-    //TODO: Replace this with something like 'filename' for use in Resources.Load()?
-    //Which one is more brittle? Guid or filename? Maybe guid is indeed best and keep as is?
+    //MAYBE: Replace this concept with something like 'filename' for use in Resources.Load()?
+    //Which one is more brittle? Guid or filename? Maybe guid is indeed best and keep as is.
     [AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
     public class ReferenceByGuidAttribute : Attribute
     {
